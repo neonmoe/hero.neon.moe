67da2120b3932efb762df2e5d97eb9fe9709fd99
excitedneon <excitedneon@gmail.com>
2017-01-16 03:30:26 +0200

    Added Pug.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var app = express();
 
-app.get("/*", function(req, res, next) {
-    res.send("This HERO-like RPG sheet manager is still under construction.");
+app.set("view engine", "pug");
+
+app.get("/", function(req, res, next) {
+    res.render("index", {title: "RPG Sheet Manager", header: "RPG Sheet Manager for HERO-likes", description: "This HERO-like RPG sheet manager is still under construction."});
 });
 
 app.listen(8863, function() {
