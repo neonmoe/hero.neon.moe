dc366611b8129714136fd775181829cdc31c4efd
excitedneon <excitedneon@gmail.com>
2017-01-16 03:44:31 +0200

    Moved CSS to an external file and made JS more ES6-y.

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ var app = express();
 
 app.set("view engine", "pug");
 
-app.get("/", function(req, res, next) {
-    res.render("index", {title: "RPG Sheet Manager", header: "RPG Sheet Manager for HERO-likes", description: "This HERO-like RPG sheet manager is still under construction."});
+app.get("/", (req, res, next) => {
+    res.render("index", {
+      title: "RPG Sheet Manager",
+      header: "RPG Sheet Manager for HERO-likes",
+      description: "This HERO-like RPG sheet manager is still under construction."
+    });
 });
 
-app.listen(8863, function() {
+app.listen(8863, _ => {
     console.log("Listening to port 8863...");
 });
