6195adf76d451976bdbf15defef14cd55c7cf5a0
teascade <teascade@gmail.com>
2017-01-31 23:20:23 +0200

    Added tabs and filled the tabs with stuff

---
+++ views/sheet-tabs.js
@@ -0,0 +1,37 @@
+const defaultTab = "lore";
+
+var currentTab = defaultTab;
+document.querySelector(`.tab[data-tab="${currentTab}"]`).dataset.active = true;
+document.querySelector(`.tabcontent[data-tab="${currentTab}"]`).dataset.active = true;
+
+function setCurrentTab() {
+  let tab = window.location.hash.substr(1);
+  if (document.querySelector(`.tabcontent[data-tab="${tab}"]`) != null) {
+    switchTab(tab);
+  } else {
+    console.log(`tab ${tab} does not exist!`)
+  }
+}
+
+document.getElementsByTagName("body")[0].onload = setCurrentTab;
+
+function switchTab(tabname) {
+  if (currentTab == tabname) { return; }
+
+  let currentTabButton = document.querySelector(`.tab[data-tab="${currentTab}"]`);
+  currentTabButton.dataset.active = false;
+  let tabButton = document.querySelector(`.tab[data-tab="${tabname}"]`);
+  tabButton.dataset.active = true;
+
+  let currentTabContent = document.querySelector(`.tabcontent[data-tab="${currentTab}"]`);
+  currentTabContent.dataset.active = false;
+  let tabContent = document.querySelector(`.tabcontent[data-tab="${tabname}"]`);
+  tabContent.dataset.active = true;
+
+  currentTab = tabname;
+
+  let hash = (tabname != defaultTab) ? "#" + tabname : "";
+  window.history.pushState(window.document.title, window.document.title,
+    window.location.href.split('#')[0] + hash);
+
+}
